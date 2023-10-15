0dfe14091c59133b6d8853818940a9c67c492b0d
Kuloud <kuloud@outlook.com>
2023-10-15 17:46:57 +0800

    fix: add packageId

--- flutter_package_tamatem_plus/lib/api/model/pojos/inventory_item.dart
+++ flutter_package_tamatem_plus/lib/api/model/pojos/inventory_item.dart
@@ -7,28 +7,29 @@ class InventoryItem {
   String? name;
   String? playerFullName;
   String? playerSerialNumber;
+  String? packageId;
 
-  InventoryItem({
-    this.codeValue,
-    this.gamePlayerId,
-    this.id,
-    this.isRedeemed,
-    this.isVerified,
-    this.name,
-    this.playerFullName,
-    this.playerSerialNumber,
-  });
+  InventoryItem(
+      {this.codeValue,
+      this.gamePlayerId,
+      this.id,
+      this.isRedeemed,
+      this.isVerified,
+      this.name,
+      this.playerFullName,
+      this.playerSerialNumber,
+      this.packageId});
 
   factory InventoryItem.fromJson(Map<String, dynamic> json) => InventoryItem(
-        codeValue: json["code_value"],
-        gamePlayerId: json["game_player_id"],
-        id: json["id"],
-        isRedeemed: json["is_redeemed"],
-        isVerified: json["is_verified"],
-        name: json["name"],
-        playerFullName: json["player_full_name"],
-        playerSerialNumber: json["player_serial_number"],
-      );
+      codeValue: json["code_value"],
+      gamePlayerId: json["game_player_id"],
+      id: json["id"],
+      isRedeemed: json["is_redeemed"],
+      isVerified: json["is_verified"],
+      name: json["name"],
+      playerFullName: json["player_full_name"],
+      playerSerialNumber: json["player_serial_number"],
+      packageId: json["package_id"]);
 
   Map<String, dynamic> toJson() => {
         "code_value": codeValue,
@@ -39,5 +40,6 @@ class InventoryItem {
         "name": name,
         "player_full_name": playerFullName,
         "player_serial_number": playerSerialNumber,
+        "package_id": packageId
       };
 }
